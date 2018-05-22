8e717e66b95e70b5949eccb7613b37ce4828bc13
root <root@chef-workstation.dexter.com.br>
2018-05-21 21:59:03 -0300

    Instalacao e configuracao do servidor apache em debian 9 e Centos 7

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'webserver-alexandresvieira'
+maintainer 'alexandresvieira DevOps'
+maintainer_email 'alexandre.svieira@github.com'
+license 'All Rights Reserved'
+description 'Servidor Apache'
+long_description 'Configuracao e Instalacao do servidor Apache'
+version '0.1.2'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+source_url "https://github.com/alexandresvieira/webserver-alexandresvieira"
---
+++ recipes/default.rb
@@ -0,0 +1,3 @@
+include_recipe 'webserver-alexandresvieira::install'
+include_recipe 'webserver-alexandresvieira::configure'
+include_recipe 'webserver-alexandresvieira::service'
